b6563eb3554af607326578ebc3981fea75ba1a67
Jeet4103 <jeetpatel4103.com>
2025-02-17 16:33:21 +0530

    first project

--- django-real-estate-main/real_estate/wsgi.py
+++ django-real-estate-main/real_estate/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for real_state project.
+WSGI config for real_estate project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_state.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_estate.settings')
 
 application = get_wsgi_application()
